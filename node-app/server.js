3e53f508f6a12e54adf0bbf0a4432ec30ab862d7
nishwanth23 <149841176+nishwanth23@users.noreply.github.com>
2025-02-12 16:52:19 +0530

    Update server.js

--- node-app/server.js
+++ node-app/server.js
@@ -7,7 +7,7 @@ mongoose.connect('mongodb://mongodb-service:27017/mydatabase', { useNewUrlParser
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Hello from Nishwanth app running in Kubernetes!');
+    res.send('Hello from Nishwanth app running in my local Kubernetes!');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
